5bf36233eed91a580f43f9839e0016650df02efb
philipstubbs13 <philipstubbs13@gmail.com>
2020-04-11 03:38:06 -0500

    chart

--- application/static/js/historicalComparisonChart.js
+++ application/static/js/historicalComparisonChart.js
@@ -1,11 +1,11 @@
-const svgWidth = 1000;
-const svgHeight = 600;
+const svgWidth = 900;
+const svgHeight = 450;
 
 const margin = {
-  top: 20,
+  top: 10,
   right: 40,
   bottom: 100,
-  left: 190
+  left: 160
 };
 
 const axisLabels = {
@@ -18,52 +18,42 @@ const axisLabels = {
 const chartWidth = svgWidth - margin.left - margin.right;
 const chartHeight = svgHeight - margin.top - margin.bottom;
 
-const url = `${api_base_url}/fires_historical`;
+const api_url = `${api_base_url}/fires_historical`;
 
-const drawChart = () => {
+const drawHistoricalComparisonChart = () => {
 
   // Choose the initial x-axis and y-axis to display.
   let chosenXAxis = axisLabels.year;
   let chosenYAxis = axisLabels.area_burned_acres;
 
   // Create an SVG wrapper, append an SVG group that will hold the chart,
-  // and shift the latter by left and top margins.
+  // and shift by left and top margins.
   const svg = d3.select("#historical-comparison-chart")
     .append("svg")
     .attr("class", "svg")
     .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`)
-  // using viewBox is what makes the svg/chart responsive:
-  // https://medium.com/@louisemoxy/a-simple-way-to-make-d3-js-charts-svgs-responsive-7afb04bc2e4b
-
 
   // Append an SVG group.
   const chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   // Retrieve data from the api endpoint and execute everything below.
-  d3.json(url).then((data, err) => {
+  d3.json(api_url).then((data, err) => {
     if (err) throw err;
 
     // Parse data/cast as numbers.
     data.result.forEach((data) => {
       data.area_burned_acres = +data.area_burned_acres;
       data.fatalities = +data.fatalities;
-      data.year = +data.year;
       data.homes_destroyed = +data.homes_destroyed;
     });
 
-    console.log(data.result)
-
     // Create scale functions.
-    let xLinearScale = d3.scaleLinear()
-      .domain([d3.min(data.result, d => d[chosenXAxis]), d3.max(data.result, d => d[chosenXAxis])])
-      .range([0, chartWidth]);
-    let yLinearScale = d3.scaleLinear()
-      .domain(d3.extent(data.result, d => d[chosenYAxis]))
-      .range([chartHeight, 0]);
+    let xTimeScale = xScale(data, chosenXAxis, chartWidth);
+    let yLinearScale = yScale(data, chosenYAxis, chartHeight);
 
     // Create initial axis functions.
-    const bottomAxis = d3.axisBottom(xLinearScale);
+    const bottomAxis = d3.axisBottom(xTimeScale).tickFormat(d3.format("y"))
     const leftAxis = d3.axisLeft(yLinearScale);
 
     // Append axes to the chart.
@@ -78,23 +68,11 @@ const drawChart = () => {
       .data(data.result)
       .enter()
       .append("circle")
-      .attr("cx", d => xLinearScale(d[chosenXAxis]))
+      .attr("cx", d => xTimeScale(d[chosenXAxis]))
       .attr("cy", d => yLinearScale(d[chosenYAxis]))
-      .attr("r", "15")
-      .attr("fill", "blue")
+      .attr("r", "9")
+      .attr("fill", "orangered")
       .attr("opacity", ".5")
-
-    // let circlesText = chartGroup.selectAll("text.text-circles")
-    //   .data(data)
-    //   .enter()
-    //   .append("text")
-    //   .classed("text-circles", true)
-    //   .text(d => d.abbr)
-    //   .attr("x", d => xLinearScale(d[chosenXAxis]))
-    //   .attr("y", d => yLinearScale(d[chosenYAxis]))
-    //   .attr("dy", 5)
-    //   .attr("text-anchor", "middle")
-    //   .attr("font-size", "10px")
 
 
     // Create group for x-axis labels.
@@ -109,38 +87,19 @@ const drawChart = () => {
       .classed("active", true)
       .text("Year");
 
-
     // Create group for y-axis labels.
     const yaxisLabelsGroup = chartGroup.append("g")
       .attr("transform", "rotate(-90)")
 
     // Create the y-axis labels.
-    const areaBurnedAcresLabel = yaxisLabelsGroup.append("text")
-      .attr("y", 0 - margin.left + 125)
-      .attr("x", 0 - (chartHeight / 2))
-      .attr("dy", "1em")
-      .attr("value", axisLabels.area_burned_acres)
-      .classed("active", true)
-      .text("Area Burned (Acres)");
+    const areaBurnedAcresLabel = createYAxisLabel(yaxisLabelsGroup, margin, 65, chartHeight, axisLabels.area_burned_acres, "active", true, "Area Burned (Acres)");
 
-    const fatalitiesLabel = yaxisLabelsGroup.append("text")
-      .attr("y", 0 - margin.left + 100)
-      .attr("x", 0 - (chartHeight / 2))
-      .attr("dy", "1em")
-      .attr("value", axisLabels.fatalities)
-      .classed("active", true)
-      .text("Fatalities");
+    const fatalitiesLabel = createYAxisLabel(yaxisLabelsGroup, margin, 45, chartHeight, axisLabels.fatalities, "inactive", true, "Fatalities");
 
-    const homesDestroyedLabel = yaxisLabelsGroup.append("text")
-      .attr("y", 0 - margin.left + 75)
-      .attr("x", 0 - (chartHeight / 2))
-      .attr("dy", "1em")
-      .attr("value", axisLabels.homes_destroyed)
-      .classed("active", true)
-      .text("Area Burned (Acres)");
+    const homesDestroyedLabel = createYAxisLabel(yaxisLabelsGroup, margin, 25, chartHeight, axisLabels.homes_destroyed, "inactive", true, "Homes Destroyed");
 
     // Create/update tooltip for each circle in the circles group.
-    // circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
+    circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
 
 
     // Event listener for when y-axis label is clicked.
@@ -154,31 +113,20 @@ const drawChart = () => {
           chosenYAxis = value;
 
           // Updates y scale for new data.
-          yLinearScale = d3.scaleLinear()
-            .domain(d3.extent(data.result, d => d[chosenYAxis]))
-            .range([chartHeight, 0]);
+          let yLinearScale = yScale(data, chosenYAxis, chartHeight);
 
-          // Updates x-axis with transition.
-          const yAxis = d3.axisLeft(yLinearScale);
+          // Updates y-axis with transition.
+          yAxis = renderYAxis(yLinearScale, yAxis);
 
-          yAxis.transition()
-            .duration(1000)
-            .call(yAxis);
-
-          // Updates circles with new x values.
-          circlesGroup.transition()
-            .duration(2000)
-            .attr("cy", d => yLinearScale(d[chosenYAxis]))
+          // Updates circles with new y values.          
+          circlesGroup = renderCircles(circlesGroup, yLinearScale, chosenYAxis);
 
           // Updates tooltips with new info.
-          // circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
-
-          // Updates circles text.
-          // circlesText = renderCirclesText(circlesText, xLinearScale, yLinearScale, chosenXAxis, chosenYAxis);
+          circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
 
           // Change classes to change bold text for y-axis labels.
           switch (chosenYAxis) {
-            case axisLabels.homesDestroyedLabel:
+            case axisLabels.homes_destroyed:
               homesDestroyedLabel
                 .classed("active", true)
                 .classed("inactive", false);
@@ -189,7 +137,7 @@ const drawChart = () => {
                 .classed("active", false)
                 .classed("inactive", true);
               break;
-            case axisLabels.fatalitiesLabel:
+            case axisLabels.fatalities:
               homesDestroyedLabel
                 .classed("active", false)
                 .classed("inactive", true);
@@ -200,7 +148,7 @@ const drawChart = () => {
                 .classed("active", false)
                 .classed("inactive", true);
               break;
-            case axisLabels.areaBurnedAcresLabel:
+            case axisLabels.area_burned_acres:
               homesDestroyedLabel
                 .classed("active", false)
                 .classed("inactive", true);
@@ -224,7 +172,107 @@ const drawChart = () => {
           }
         }
       });
-  }).catch((error) => console.log(error));
+  }).catch((error) => console.error(error));
 }
 
-drawChart();
+// Function used for setting the x-axis scale.
+const xScale = (data, chosenXAxis, chartWidth) => {
+  const xTimeScale = d3.scaleTime()
+    .domain(d3.extent(data.result, function (d) {
+      return d[chosenXAxis];
+    }))
+    .range([0, chartWidth]);
+
+  return xTimeScale;
+}
+
+// Function used to create y-axis label.
+const createYAxisLabel = (yaxisLabelsGroup, margin, y, chartHeight, value, activeClass, active, text) => {
+  const label = yaxisLabelsGroup.append("text")
+    .attr("y", 0 - margin.left + y)
+    .attr("x", 0 - (chartHeight / 2))
+    .attr("dy", "1em")
+    .attr("value", value)
+    .classed(activeClass, active)
+    .text(text);
+
+  return label;
+}
+
+// Function used for setting y-axis scale.
+const yScale = (data, chosenYAxis, chartHeight) => {
+  // create scales
+  let yLinearScale = d3.scaleLinear()
+    .domain([d3.min(data.result, d => d[chosenYAxis]), d3.max(data.result, d => d[chosenYAxis])])
+    .range([chartHeight, 0]);
+
+  return yLinearScale;
+}
+
+// Function used for updating y-axis when clicking on the y-axis label.
+const renderYAxis = (newYScale, yAxis) => {
+  const leftAxis = d3.axisLeft(newYScale);
+
+  yAxis.transition()
+    .duration(1000)
+    .call(leftAxis);
+
+  return yAxis;
+}
+
+// Function used for updating circles group with a transition to new circles.
+const renderCircles = (circlesGroup, newYScale, chosenYAxis) => {
+  circlesGroup.transition()
+    .duration(2000)
+    .attr("cy", d => newYScale(d[chosenYAxis]))
+
+  return circlesGroup;
+}
+
+// Function used to determine which y-axis label to display inside the tooltip.
+const chooseYLabel = (chosenYAxis) => {
+  switch (chosenYAxis) {
+    case axisLabels.area_burned_acres:
+      return "Area Burned (Acres):";
+    case axisLabels.fatalities:
+      return "Fatalities:";
+    case axisLabels.homes_destroyed:
+      return "Homes Destroyed:";
+    default:
+      return "Area Burned (Acres):";
+  }
+}
+
+// Function used for updating circles group with new tooltip.
+const updateToolTip = (chosenXAxis, chosenYAxis, circlesGroup) => {
+
+  const xLabel = "Year:";
+
+  const yLabel = chooseYLabel(chosenYAxis);
+
+  const numberFormat = d3.format(",");
+
+  const toolTip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([80, -60])
+    .html((d) => {
+      return (`
+        ${d.name}<br>
+        ${xLabel} ${d[chosenXAxis]}<br>
+        ${yLabel} ${numberFormat(d[chosenYAxis])}<br>
+      `);
+    });
+
+  circlesGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function (d) {
+    toolTip.show(d, this);
+  })
+    .on("mouseout", function (data, index) {
+      toolTip.hide(data);
+    });
+
+  return circlesGroup;
+}
+
+drawHistoricalComparisonChart();
